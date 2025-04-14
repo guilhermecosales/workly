a62ce29b0109af35c6863d594ab317d5fd7117d8
Guilherme Costa Sales <guilhermecosales@gmail.com>
2025-04-14 20:37:48 -0300

    feat: move mapping functions to separate mapper file for better organization

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/guilhermecosales/workly/schemas"
 	"net/http"
 )
 
@@ -38,31 +37,4 @@ func CreateOpening(context *gin.Context) {
 
 	// Send success response
 	sendSuccessResponse(context, "Create Opening successfully", openingResponse)
-}
-
-func mapToOpening(request CreateOpeningRequest) schemas.Opening {
-	logger.Debugf("Mapping request to Opening schema: %+v", request)
-	return schemas.Opening{
-		Role:     request.Role,
-		Company:  request.Company,
-		Location: request.Location,
-		Remote:   *request.Remote,
-		Link:     request.Link,
-		Salary:   request.Salary,
-	}
-}
-
-func mapToOpeningResponse(opening schemas.Opening) schemas.OpeningResponse {
-	logger.Debugf("Mapping Opening schema to response: %+v", opening)
-	return schemas.OpeningResponse{
-		Id:        opening.ID,
-		Role:      opening.Role,
-		Company:   opening.Company,
-		Location:  opening.Location,
-		Remote:    opening.Remote,
-		Link:      opening.Link,
-		Salary:    opening.Salary,
-		CreatedAt: opening.CreatedAt,
-		UpdatedAt: opening.UpdatedAt,
-	}
 }
---
+++ handler/mapper.go
@@ -0,0 +1,30 @@
+package handler
+
+import "github.com/guilhermecosales/workly/schemas"
+
+func mapToOpening(request CreateOpeningRequest) schemas.Opening {
+	logger.Debugf("Mapping request to Opening schema: %+v", request)
+	return schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+}
+
+func mapToOpeningResponse(opening schemas.Opening) schemas.OpeningResponse {
+	logger.Debugf("Mapping Opening schema to response: %+v", opening)
+	return schemas.OpeningResponse{
+		Id:        opening.ID,
+		Role:      opening.Role,
+		Company:   opening.Company,
+		Location:  opening.Location,
+		Remote:    opening.Remote,
+		Link:      opening.Link,
+		Salary:    opening.Salary,
+		CreatedAt: opening.CreatedAt,
+		UpdatedAt: opening.UpdatedAt,
+	}
+}
